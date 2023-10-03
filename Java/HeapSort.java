54ac44a04d01ca8d75e61225badba3a48f2f1d89
GenDelta <ankush.dutta.kol@gmail.com>
2023-10-03 12:12:17 +0530

    Made modifications and added Python and Java programs

--- Java/HeapSort.java
+++ Java/HeapSort.java
@@ -62,7 +62,6 @@ public class HeapSort {
 	public static void main(String args[])
 	{
 		int arr[] = { 12, 11, 13, 5, 6, 7 };
-		int N = arr.length;
 
 		// Function call
 		HeapSort ob = new HeapSort();
