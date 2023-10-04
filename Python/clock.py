8ceb40bb1846cf39c83a845bdd8bf69e4dfc680c
NawinKumarSharma <nawinsharma60@gmail.com>
2023-10-04 16:45:40 +0530

    :memo: added a python program of digital clock using tkinter

---
+++ Python/clock.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%I:%M:%S %p")
+    time_label.config(text=time_string)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
+
+    window.after(1000, update)
+
+
+window = Tk()
+
+time_label = Label(window, font=("Arial", 50), fg="#00FF00", bg="black")
+time_label.pack()
+
+day_label = Label(window, font=("Ink Free", 25))
+day_label.pack()
+
+date_label = Label(window, font=("Ink Free", 30))
+date_label.pack()
+
+update()
+
+window.mainloop()
